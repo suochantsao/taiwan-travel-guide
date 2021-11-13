e2877e535813e0a40b7147f91a193f30067725dc
suochantsao <suochantsao@gmail.com>
2021-11-13 17:36:28 +0800

    專案資料夾結構建立和更新

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 // import { Route, Switch } from "react-router-dom";
 
-import "./App.css";
-
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <p>hello</p>
+    </div>
+  );
 }
 
 export default App;

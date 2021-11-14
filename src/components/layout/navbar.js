ce8248ea68843c88fe7697b4537654ba6e254362
suochantsao <suochantsao@gmail.com>
2021-11-14 20:03:10 +0800

    navbar 設定完成

---
+++ src/components/layout/navbar.js
@@ -0,0 +1,25 @@
+// Router
+import { Link } from "react-router-dom";
+
+function Navigation() {
+  return (
+    <header>
+      <h1>
+        <Link to="/"> WEB LOGO </Link>
+      </h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/attraction"> 景點探索 </Link>
+          </li>
+          <li>熱門住宿</li>
+          <li>最夯美食</li>
+          <li>活動與展覽</li>
+          <li>登入 / 註冊</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Navigation;

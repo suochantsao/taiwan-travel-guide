895cec818c9477116a03ae68988d7f1ad1ecdca7
suochantsao <suochantsao@gmail.com>
2021-11-18 19:47:03 +0800

    設定 layout 樣式

---
+++ src/components/layout/layout.js
@@ -0,0 +1,12 @@
+import Navigation from "@/components/layout/navbar.js";
+
+function Layout(props) {
+  return (
+    <div>
+      <Navigation />
+      <main></main>
+    </div>
+  );
+}
+
+export default Layout;
